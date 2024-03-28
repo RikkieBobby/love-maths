c3183609991975c66a7df368603a059010123ccc
richie nolan <richienolan23@gmail.com>
2024-03-28 15:21:59 +0000

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-/**
- * function to loop through each button when clicked
- */
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -11,24 +10,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
-
-
-
-
 /**
- * main game loop, called when the script is first loaded 
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
+function runGame(gameType) {
 
-function runGame() {
-    let num1 = math.floor(math.random() * 25) + 1;
-    let num2 = math.floor(math.random() * 25) + 1;
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -43,12 +50,16 @@ function incrementScore() {
 
 }
 
-function incrementWrongAsnwer() {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
@@ -56,9 +67,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
-}
-
-function displayDivisionQuestion() {
-
+    
 }
