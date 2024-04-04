f29a284c4bbd13c2c6e03b609794a5ccea772ebc
richie nolan <richienolan23@gmail.com>
2024-04-04 10:14:36 +0000

    add functions calculateCorrectAnswer and checkAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,8 +38,22 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Checks the answer agaimst the first element in 
+ * the returned claculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got it right!")
+    } else {
+        alert(`Sorry, you answered ${userAnswer}, the correct answer is ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
